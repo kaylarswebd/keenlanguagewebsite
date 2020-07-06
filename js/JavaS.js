d921281ab022b9d35028e5600bcdec20b71e71be
kaylarswebd <54419990+kaylarswebd@users.noreply.github.com>
2020-07-05 22:42:18 -0500

    changed color of footer h5

--- js/JavaS.js
+++ js/JavaS.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //Get the button:
 mybutton = document.getElementById("myBtn");
 
@@ -17,24 +16,4 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-=======
-//Get the button:
-mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
->>>>>>> 7f0a095643c99e66a0da17ca5f4dc90ad930fd42
 }
