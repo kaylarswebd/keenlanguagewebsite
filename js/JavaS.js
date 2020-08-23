b0745cd5c91b3faf383ff07c94430f783234735b
kaylarswebd <54419990+kaylarswebd@users.noreply.github.com>
2020-08-22 22:22:52 -0500

    fixed javascript problem greet and time

--- js/JavaS.js
+++ js/JavaS.js
@@ -1,11 +1,7 @@
-const { nodeName } = require("jquery");
-
-//Get the button:
+//BACK TO TOP
 mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
-
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
@@ -14,8 +10,36 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+//WEATHER
+! function (d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (!d.getElementById(id)) {
+    js = d.createElement(s);
+    js.id = id;
+    js.src = 'https://weatherwidget.io/js/widget.min.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }
+}(document, 'script', 'weatherwidget-io-js');
+
+//GREETING
+var today = new Date()
+var curHr = today.getHours()
+
+if (curHr >= 1 && curHr < 6) {
+    document.getElementById("greeting").innerHTML = 'What are you doing up this early?';
+} else if (curHr >= 6 && curHr <= 12) {
+    document.getElementById("greeting").innerHTML = 'Good Morning and welcome!';
+} else if (curHr >= 12 && curHr < 17) {
+    document.getElementById("greeting").innerHTML = 'Good Afternoon and welcome!';
+} else {
+    document.getElementById("greeting").innerHTML = 'Good Evening and welcome!';
+}
+
+//TIME AND DATE
+var d = new Date();
+document.getElementById("timedate").innerHTML = 'Date and time = ' + d;
